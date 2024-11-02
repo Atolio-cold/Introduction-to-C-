79c11931e410ea240ffe044c7b118dd0fcf8f450
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-11-02 21:14:55 +0100

    Add files via upload

---
+++ NumbersInC#/Program.cs
@@ -0,0 +1,58 @@
+﻿// int a = 5;
+// int b = 4;
+// int c = 2;
+// int d = a + b * c;
+// Console.WriteLine(d);
+
+// int a = 5;
+// int b = 4;
+// int c = 2;
+// int d = (a + b) * c;
+// Console.WriteLine(d);
+
+// int x = 13;
+// int y = 9;
+// int z = 10;
+// int suma = x + y;
+// Console.WriteLine(suma);
+// int iloraz = (x + y) % z;
+// int reszta = (x + y) / z;
+// Console.WriteLine($"Iloraz wynosi: {iloraz}");
+// Console.WriteLine($"Reszta wynosi: {reszta}");
+
+// int max = int.MaxValue;
+// int min = int.MinValue;
+// Console.WriteLine($"The range of integers is {min} to {max}");
+
+// int what = max + 3;
+// Console.WriteLine($"An example of overflow: {what}");
+
+
+// double a = 5;
+// double b = 4;
+// double c = 2;
+// double d = (a + b) / c;
+// Console.WriteLine(d);
+
+// double max = double.MaxValue;
+// double min = double.MinValue;
+// Console.WriteLine($"The range of double is {min} to {max}");
+
+// double third = 1.0 / 3.0;
+// Console.WriteLine(third);
+
+// decimal min = decimal.MinValue;
+// decimal max = decimal.MaxValue;
+// Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+double a = 1.0;
+double b = 3.0;
+Console.WriteLine(a / b);
+
+decimal c = 1.0M;
+decimal d = 3.0M;
+Console.WriteLine(c / d);
+
+double radius = 2.50;
+double area = Math.PI * radius * radius;
+Console.WriteLine(area);
