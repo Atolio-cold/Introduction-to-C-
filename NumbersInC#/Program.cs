e619936e5223a25077280092cd6a1122b0445526
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-11-02 19:15:55 +0100

    Add files via upload

---
+++ NumbersInC#/Program.cs
@@ -0,0 +1,21 @@
+﻿// int a = 5;
+// int b = 4;
+// int c = 2;
+// int d = a + b * c;
+// Console.WriteLine(d);
+
+int a = 5;
+int b = 4;
+int c = 2;
+int d = (a + b) * c;
+Console.WriteLine(d);
+
+int x = 13;
+int y = 9;
+int z = 10;
+int suma = x + y;
+Console.WriteLine(suma);
+int iloraz = (x + y) % z;
+int reszta = (x + y) / z;
+Console.WriteLine($"Iloraz wynosi: {iloraz}");
+Console.WriteLine($"Reszta wynosi: {reszta}");
